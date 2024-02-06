a07bc605374f3708ea565cbafc5170897e65efdb
Bruno Ponsoni Costa <42554984+psonybruno@users.noreply.github.com>
2024-02-06 15:59:12 -0300

    Aula 01

---
+++ README.md
@@ -0,0 +1,12 @@
+# NLW React Native
+
+## Aula 01
+### Instalações
+```
+- npx create-expo-app --template -> Inicialização ( Navigation(Typescript) )
+- npm i nativewind@2.0.11
+- npm i tailwindcss@3.3.2 -D
+- npx tailwindcss init 
+- npx expo install expo-font @expo-google-fonts/inter
+- npm i clsx
+```
---
+++ src/app/index.tsx
@@ -0,0 +1,32 @@
+import { FlatList, View } from 'react-native';
+
+import Header from '@/components/header';
+
+import { CATEGORIES } from '@/utils/data/products';
+import { CategoryButton } from '@/components/category-button';
+import { useState } from 'react';
+
+export default function Home()
+{
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelected(selectedCategory: string) {
+    setCategory(selectedCategory)
+  }
+
+  return (
+    <View className='flex-1 pt-8'>
+        <Header title='Faça seu pedido' cartQuantityItems={3} />
+
+      <FlatList 
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelected(item)} /> }
+        horizontal
+        className='max-h-10 mt-5'
+        showsHorizontalScrollIndicator
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20}}
+      />
+    </View>
+  )
+}
